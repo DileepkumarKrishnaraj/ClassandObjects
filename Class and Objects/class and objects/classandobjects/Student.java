85f5ae1f79c84b30ceea185f39e88d56d0f23e34
DileepkumarKrishnaraj <72642136+DileepkumarKrishnaraj@users.noreply.github.com>
2022-07-26 11:05:40 +0530

    Add files via upload

---
+++ Class and Objects/class and objects/classandobjects/Student.java
@@ -0,0 +1,13 @@
+package classandobjects;
+
+public class Student {
+	int stuid;
+	String stuname;
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println(s1.stuid);
+		System.out.println(s1.stuname);
+	}
+
+}
